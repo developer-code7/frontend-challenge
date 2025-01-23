283097d07cd6b44f0f02e03dcfe88cb2fd957ead
Kapil Jain <kapiljain46245@gmail.com>
2025-01-23 13:49:32 +0530

    modified:   src/assets/index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 export { default as claim } from "./Claims.png";
 export { default as employee } from "./Employee.png";
 export { default as leave } from "./Leaves.png";
-export { default as payroll } from "./payroll.png";
+export { default as payroll } from "./Payroll.png";
 export { default as more } from "./more.png";
 export { default as home } from "./home.png";
 export { default as building } from "./building.png";
@@ -11,13 +11,9 @@ export { default as searchuserlogo } from "./searchuserlogo.png";
 export { default as money } from "./money.png";
 export { default as human } from "./human.png";
 export { default as bulkupload } from "./bulkupload.png";
-
 export { default as excellogo } from "./excellogo.png";
-
 export { default as download } from "./download.png";
 export { default as upload } from "./upload.png";
 export  { default as updown } from "./updown.png";
-
-
 export  { default as search } from "./search.png";
 export  { default as notification } from "./notification.png";
